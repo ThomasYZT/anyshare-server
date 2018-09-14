96c88971cfe7d70ce5e2dcdb4b441ece42101439
yangzetao <624524240@qq.com>
2018-09-14 14:55:05 +0800

    添加生产环境变量命令

---
+++ config/env/production.js
@@ -0,0 +1,15 @@
+/**
+ * 生产环境配置文件
+ */
+
+export const config = {
+  db: {
+    "username": "thomasyzt",
+    "password": "5L7PtXGAerX8p2Ax",
+    "database": "anyshare",
+    "host": "127.0.0.1",
+    "port": 3306,
+    "dialect": "mysql",
+    "operatorsAliases": false
+  }
+} 
